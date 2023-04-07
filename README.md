b8ef394dde7f2ed99e0d9096dc2a18049371838c
huseyingulec <itshuseyingulec@gmail.com>
2023-04-07 23:32:19 +0200

    main update
    in this commit, I have updated mostly js file, added new button and added new feature to add current task to to the leads array

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lead's tracker chrome extension
+    This is an extension that saves tabs and inputs, it does basic function that google readlist does. I am trying to make a personal google bookmark/readlist.
+
+## What I have done;
+    In this project, I have learnt some concept of JS such as const variable, difference between const and let variables, addEventListener, innerHTML, input.value, function parameters, difference between parameters and arguments, template strings, most importantly localStorage, setting and getting item in localStorage, JSON objects, JSON.parse and JSON.stringify, objects in arrays.
--- index.js
+++ index.js
@@ -2,23 +2,68 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+if (leadsFromLocalStorage) { // checks the localStorage and renders them into the page
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){ // ask the infos of active and current windows 
+        myLeads.push(tabs[0].url) // adds the url of current tab
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) ) // adds the url to the localStorage
+        render(myLeads)
+    })
 })
 
-function renderLeads() {
+function render(leads) { // renders item as a list and anchor tag
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+            <ul>
+                <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
-            </li>
+                </li>
+            </ul>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
+
+
+inputBtn.addEventListener("click", function() { // adds inputs to the array
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
+
+
+
+let clicks = 0; 
+
+deleteBtn.addEventListener("click", function() { // counts the clicks and execute different functions
+  clicks++;
+  setTimeout(function() {
+    if (clicks === 1) {
+      
+      myLeads = leadsFromLocalStorage // gets the items from the myLeads localStorage and turns them as an array
+      myLeads.pop() // deletes the latest element from the array
+      localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+      render(myLeads)
+
+
+    } else if (clicks === 2 && window.confirm("Do you really want to delete all?")) { // shows a confirm windows to delete all leads
+      
+      localStorage.clear()
+      myLeads = []
+      render(myLeads)
+    }
+    clicks = 0;
+  }, 300); // Delay between clicks
+});
